788ea300f9edd5b689af96155b72492bb800fdb3
ManishBisht <manishbisht9711@gmail.com>
2021-09-17 20:24:01 +0530

    count pair with given sum  [O (N2)]

---
+++ countsumpair.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int s;
+    cin >> s;
+
+    int *arr = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] + arr[j] == s)
+            {
+                count++;
+            }
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
