c0105964cfef84e2095d0071e269cb30cdd0b5b9
ManishBisht <manishbisht9711@gmail.com>
2021-09-21 14:06:24 +0530

    reverse singly listed (iterative way)

--- singlylinkedlist.cpp
+++ singlylinkedlist.cpp
@@ -38,11 +38,12 @@ void insertattail(node *&head, int val)
 void display(node *head)
 {
     node *temp = head;
-    while (temp->next)
+    while (temp != NULL)
     {
-        cout << temp->data;
+        cout << temp->data << "->";
         temp = temp->next;
     }
+    cout << "NULL \n";
 }
 
 void insertathead(node *head, int val)
@@ -103,6 +104,24 @@ void deletion(node *head, int val)
     delete todelete;
 }
 
+node *reverse(node *head)
+{
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr;
+
+    while (currptr != NULL)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+
+        prevptr = currptr;
+        currptr = nextptr;
+    }
+
+    return prevptr;
+}
+
 int main()
 {
     node *head = NULL;
@@ -110,6 +129,9 @@ int main()
     insertattail(head, 2);
     insertattail(head, 3);
     insertattail(head, 4);
+    display(head);
+    // cout << search(head, 3);
 
-    cout << search(head, 3);
+    node *newhead = reverse(head);
+    display(newhead);
 }
