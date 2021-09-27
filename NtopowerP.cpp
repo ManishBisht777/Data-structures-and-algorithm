fbb1f6145ff252866a96b2367748ea66f7041027
ManishBisht <manishbisht9711@gmail.com>
2021-09-27 13:03:38 +0530

    N to Power P using reccursion

---
+++ NtopowerP.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int powerofn(int n, int p)
+{
+    if (p == 0)
+    {
+        return 1;
+    }
+
+    int prevpower = powerofn(n, p - 1);
+
+    return n * prevpower;
+}
+int main()
+{
+    int n, p;
+    cin >> n >> p;
+    cout << powerofn(n, p);
+}
