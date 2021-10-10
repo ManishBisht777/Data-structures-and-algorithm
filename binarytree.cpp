e614f87494a86c40886b5357a66f057a2d540744
ManishBisht <manishbisht9711@gmail.com>
2021-10-10 12:08:20 +0530

    right view of binary tree

--- binarytree.cpp
+++ binarytree.cpp
@@ -296,6 +296,43 @@ bool isbalanced2(node *root, int *height)
         return false;
     }
 }
+
+void rightview(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        int n = q.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            node *curr = q.front();
+            q.pop();
+
+            if (i == n - 1)
+            {
+                cout << curr->data << " ";
+            }
+
+            if (curr->left != NULL)
+            {
+                q.push(curr->left);
+            }
+
+            if (curr->right != NULL)
+            {
+                q.push(curr->right);
+            }
+        }
+    }
+}
 int main()
 {
     node *root = new node(1);
@@ -305,8 +342,8 @@ int main()
     root->left->right = new node(5);
     root->right->left = new node(6);
     root->right->right = new node(7);
-    root->right->left->left = new node(68);
-    root->right->left->left->left = new node(69);
+    // root->right->left->left = new node(68);
+    // root->right->left->left->left = new node(69);
 
     // preorder(root);
     // cout << "\n";
@@ -331,14 +368,15 @@ int main()
     // sumreplace(root);
     // preorder(root);
 
-    int height = 0;
-    if (isbalanced2(root, &height) == true)
-    {
-        cout << "true";
-    }
-    else
-    {
-        cout << "false";
-    }
-    return 0;
+    // int height = 0;
+    // if (isbalanced2(root, &height) == true)
+    // {
+    //     cout << "true";
+    // }
+    // else
+    // {
+    //     cout << "false";
+    // }
+    // return 0;
+    rightview(root);
 }
