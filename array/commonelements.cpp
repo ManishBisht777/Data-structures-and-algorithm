c33cff5496258d415389f6a72c6b160a53f9332a
ManishBisht <manishbisht9711@gmail.com>
2021-10-11 22:52:51 +0530

    common elements in 3 sorted array optimised and removed duplicates

--- array/commonelements.cpp
+++ array/commonelements.cpp
@@ -7,44 +7,62 @@ int main()
 
     int n1, n2, n3;
     cin >> n1 >> n2 >> n3;
-    int a[n1];
-    int b[n2];
-    int c[n3];
+    int A[n1];
+    int B[n2];
+    int C[n3];
     for (int i = 0; i < n1; i++)
-        cin >> a[i];
+        cin >> A[i];
 
     for (int i = 0; i < n2; i++)
-        cin >> b[i];
+        cin >> B[i];
 
     for (int i = 0; i < n3; i++)
-        cin >> c[i];
+        cin >> C[i];
 
     int i = 0, j = 0, k = 0;
 
-    while (i < n1 || j < n2 || k < n3)
+    int prev1, prev2, prev3;
+    prev1 = prev2 = prev3 = -9999;
+    while (i < n1 && j < n2 && k < n3)
     {
-        if (a[i] == b[j] && a[i] == c[k])
+        while (A[i] == prev1 && i < n1)
         {
-            cout << a[i] << " ";
-            i++, j++, k++;
-
-            if (i == n1 || j == n2 || k == n3)
-            {
-                break;
-            }
-        }
-
-        else if (a[i] < b[j])
-        {
-
             i++;
         }
-
-        else if (b[j] < c[k])
+        while (B[j] == prev2 && j < n2)
         {
             j++;
         }
-        else
+        while (C[k] == prev3 && k < n3)
+        {
             k++;
+        }
+        if (A[i] == B[j] && A[i] == C[k])
+        {
+            cout << A[i] << " ";
+            prev1 = A[i];
+            prev2 = B[j];
+            prev3 = C[k];
+            i++;
+            j++;
+            k++;
+        }
+
+        else if (A[i] < B[j])
+        {
+            prev1 = A[i];
+            i++;
+        }
+
+        else if (B[j] < C[k])
+        {
+            prev2 = B[j];
+            j++;
+        }
+        else
+        {
+            prev3 = C[k];
+            k++;
+        }
     }
 }
