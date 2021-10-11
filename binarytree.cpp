c0ea913e68f9c0893d393610567ee7adae99cbbd
ManishBisht <manishbisht9711@gmail.com>
2021-10-11 13:54:28 +0530

    lowest common ancestor

--- binarytree.cpp
+++ binarytree.cpp
@@ -524,6 +524,49 @@ int printnodesatk(node *root, node *target, int k)
     return -1;
 }
 
+bool getpath(node *root, int key, vector<int> &path)
+{
+    if (root == NULL)
+    {
+        return false;
+    }
+
+    path.push_back(root->data);
+    if (root->data == key)
+    {
+        return true;
+    }
+
+    if (getpath(root->left, key, path) || getpath(root->right, key, path))
+    {
+        return true;
+    }
+
+    path.pop_back();
+    return false;
+}
+
+int LCA(node *root, int n1, int n2)
+{
+    vector<int> path1, path2;
+
+    if (!getpath(root, n1, path1) || !getpath(root, n2, path2))
+    {
+        return -1;
+    }
+
+    int pc;
+
+    for (pc = 0; pc < path1.size() && path2.size(); pc++)
+    {
+        if (path1[pc] != path2[pc])
+        {
+            break;
+        }
+    }
+
+    return path1[pc - 1];
+}
 int main()
 {
     node *root = new node(1);
@@ -579,5 +622,8 @@ int main()
     // flatten(root);
     // inorder(root);
 
-    printnodesatk(root, root, 2);
+    // printnodesatk(root, root, 2);
+
+    int pc = LCA(root, 7, 6);
+    cout << pc;
 }
