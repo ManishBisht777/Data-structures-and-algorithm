cbc75c5059fd9f9ac0fcf88f1ab19cd0214460cf
ManishBisht <manishbisht9711@gmail.com>
2021-10-13 19:38:36 +0530

    find duplicate optimised

--- array/Findduplicate.cpp
+++ array/Findduplicate.cpp
@@ -16,8 +16,31 @@ int duplicate(int arr[], int n)
     }
 }
 
+int duplicate2(int arr[], int n)
+{
+    int max_idx = 0;
+    int curr_max = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int id = arr[i] % n;
+        arr[id] += n;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > curr_max)
+        {
+            curr_max = arr[i];
+            max_idx = i;
+        }
+        arr[i] %= n;
+    }
+    return max_idx;
+}
+
 int main()
 {
     int arr[] = {1, 3, 4, 5, 1, 6};
-    cout << duplicate(arr, 6);
+    cout << duplicate2(arr, 6);
 }
