2004891624a118c4dd93694165b1b253e7bc751b
ManishBisht <manishbisht9711@gmail.com>
2021-10-14 18:36:30 +0530

    subset or not ( not optimised)

---
+++ array/subsetornot.cpp
@@ -0,0 +1,21 @@
+//sort both array then
+// i=0,j=0
+// int count=0
+// while(j<size2)
+// {
+//     if(arr[i]==arr[j])
+//     {
+//         i++;
+//         j++;
+// count++
+//     }
+//     else{
+//         i++;
+//     }
+// }
+
+// if(count==size2)
+// {
+//     return true;
+// }
+// return false;
