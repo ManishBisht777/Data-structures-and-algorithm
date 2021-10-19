1b3689b150f2f9248ce7c4a1a72af6130d3b8480
ManishBisht <manishbisht9711@gmail.com>
2021-10-19 15:48:26 +0530

    longest substring with no repetition

---
+++ string/longestsubstring.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s = "abcdefghihaaaaaaabcdefghijklmopbb";
+    int i = 0, j = 1, ans = 0;
+    int k;
+
+    for (j = 0; j < s.size(); j++)
+    {
+        k = i;
+        while (k < j)
+        {
+            if (s[k] == s[j])
+            {
+                i = j;
+                break;
+            }
+            k++;
+        }
+        ans = max(ans, j - i + 1);
+    }
+    cout << ans;
+}
