b5ea2bc8b8f1515b452e6d201ac789861128d52d
ManishBisht <manishbisht9711@gmail.com>
2021-10-29 13:15:08 +0530

    prev smaller element

---
+++ stack/prevsmaller.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void nextsmall(int arr[], int n)
+{
+    stack<int> s;
+    for (int i = 0; i < n; i++)
+    {
+        while (!s.empty() && s.top() >= arr[i])
+        {
+            s.pop();
+        }
+
+        if (s.empty())
+        {
+            cout << "-1 ";
+        }
+        else
+        {
+            cout << s.top() << " ";
+        }
+        s.push(arr[i]);
+    }
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    nextsmall(arr, 5);
+}
