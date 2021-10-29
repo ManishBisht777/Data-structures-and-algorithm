a31a37897e2d13da50a83893344af591fff91c6c
ManishBisht <manishbisht9711@gmail.com>
2021-10-29 13:13:35 +0530

    next smaller element

---
+++ stack/nextsmaller.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void nextsmall(int arr[], int n)
+{
+    stack<int> s;
+    int *ans = new int[n];
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!s.empty() && s.top() >= arr[i])
+        {
+            s.pop();
+        }
+
+        if (s.empty())
+        {
+            ans[i] = -1;
+        }
+        else
+        {
+            ans[i] = s.top();
+        }
+        s.push(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << ans[i] << " ";
+    }
+}
+int main()
+{
+    int arr[] = {5, 4, 3, 2, 1};
+    nextsmall(arr, 5);
+}
