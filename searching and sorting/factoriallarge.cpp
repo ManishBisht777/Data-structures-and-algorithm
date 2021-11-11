0df5f5e8404a8b8c948d4115e5850b1a1c785696
ManishBisht <manishbisht9711@gmail.com>
2021-11-11 22:24:47 +0530

    factorial of a large number

---
+++ searching and sorting/factoriallarge.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> factorial(int n)
+{
+    vector<int> v;
+    v.push_back(1);
+
+    for (int i = 2; i < n; i++)
+    {
+        int carry = 0;
+        for (int j = v.size() - 1; j >= 0; j--)
+        {
+            int value = v[j] * i + carry;
+            v[j] = value % 10;
+            carry = value / 10;
+        }
+
+        while (carry > 0)
+        {
+            v.insert(v.begin(), carry % 10);
+            carry /= 10;
+        }
+    }
+
+    return v;
+}
