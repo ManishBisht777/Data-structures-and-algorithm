9f43019b3e2bb266b039b9b9443de89bc8555174
Manish Bisht <89926834+ManishBisht777@users.noreply.github.com>
2021-11-29 15:40:56 +0530

    First non-repeating character in a stream

---
+++ nonrep.cpp
@@ -0,0 +1,20 @@
+string FirstNonRepeating(string a)
+{
+    int arr[26] = {0};
+    queue<int> q;
+    for (int i = 0; i < a.length(); i++)
+    {
+        arr[a[i] - 'a']++;
+        if (arr[a[i] - 'a'] == 1)
+            q.push(a[i]);
+
+        while (!q.empty() && arr[q.front() - 'a'] != 1)
+            q.pop();
+
+        if (q.empty())
+            a[i] = '#';
+        else
+            a[i] = q.front();
+    }
+    return a;
+}
